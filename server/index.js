eb8f3c102cb19d7086de625bcac249161e11add1
divya <divu333@gmail.com>
2019-01-28 22:18:52 -0700

    setup done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const controller = require("./controller");
+const massive = require("massive");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../public/build"));
+massive(process.env.CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+  })
+  .catch(err => console.log(err));
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server listening at localhost:${port}`);
+});
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import House from "../House/House";
+
+class Dashbaord extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        Dashboard
+        <House />
+      </div>
+    );
+  }
+}
+
+export default Dashbaord;
